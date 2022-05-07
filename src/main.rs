f1af5476c04e9782655fbe489b5059161c78e674
Wink Saville <wink@saville.com>
2022-05-07 16:46:21 -0700

    feat: Make State trait generic over P
    Where P is an Protocol which would typically be
    defined as a set of Messages within the enum.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
 use custom_logger::env_logger_init;
 
-pub trait State {
+pub trait State<P> {
     fn enter(&mut self) {
         log::debug!("State: enter");
     }
-    fn process(&mut self) {
-        log::debug!("State: process");
+    fn process(&mut self, _m: &P) {
+        log::debug!("State: process:");
     }
     fn exit(&mut self) {
         log::debug!("State: exit");
     }
 }
 
+#[derive(Debug)]
+enum Protocol1 {
+    Msg1 {
+        f1: i32,
+    }
+}
+
 struct MySm {
-    cur_state: Box<dyn State + 'static>,
+    cur_state: Box<dyn State<Protocol1> + 'static>,
 }
 
 struct State1;
 
-impl State for State1 {
-    fn process(&mut self) {
-        //, msg: ProtocolType) {
-        log::debug!("State1: process");
+impl State<Protocol1> for State1 {
+    fn process(&mut self, m: &Protocol1) {
+        match m {
+            Protocol1::Msg1 { f1 } => {
+                log::debug!("State1: process m.f1={}", f1);
+            }
+        }
     }
 }
 
@@ -34,7 +44,10 @@ fn main() {
         cur_state: Box::new(state1),
     };
 
+    let msg = Protocol1::Msg1 {
+        f1: 123,
+    };
     mysm.cur_state.enter();
-    mysm.cur_state.process();
+    mysm.cur_state.process(&msg);
     mysm.cur_state.exit();
 }
