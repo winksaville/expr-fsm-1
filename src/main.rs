5ed09a8bd8bae6e866930eab3e772df26800dc98
Wink Saville <wink@saville.com>
2022-05-07 12:10:16 -0700

    feat: Define a State trait
    A State trait has 3 methods; enter, process, exit. And these methods
    can be specialized by the implementations. There is a notion of a
    state machine which has the notion of the current state. Here there is
    a structure with cur_state for which you can dynamically invoke the
    State trait methods.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
-#![feature(thread_id_value)]
-
 use custom_logger::env_logger_init;
+
+pub trait State {
+    fn enter(&mut self) {
+        log::debug!("State: enter");
+    }
+    fn process(&mut self) {
+        log::debug!("State: process");
+    }
+    fn exit(&mut self) {
+        log::debug!("State: exit");
+    }
+}
+
+struct MySm {
+    cur_state: Box<dyn State + 'static>,
+}
+
+struct State1;
+
+impl State for State1 {
+    fn process(&mut self) {
+        //, msg: ProtocolType) {
+        log::debug!("State1: process");
+    }
+}
 
 fn main() {
     env_logger_init("info");
     log::info!("Hello, world!");
+
+    let state1 = State1 {};
+    let mut mysm = MySm {
+        cur_state: Box::new(state1),
+    };
+
+    mysm.cur_state.enter();
+    mysm.cur_state.process();
+    mysm.cur_state.exit();
 }
