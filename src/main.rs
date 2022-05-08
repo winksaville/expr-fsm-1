2de19749ada59e7b40e9cda2f38ac63970efe43d
Wink Saville <wink@saville.com>
2022-05-08 16:55:53 -0700

    feat: Change methods to take msg as &P
    Add a bit of docs and remove allow(unused)

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use custom_logger::env_logger_init;
 
+// Trait for Finite State Machines
 trait StateMachine<P> {
+    /// Return the current state
     fn cur_state(&mut self) -> &mut Box<dyn State<P> + 'static>;
+    //fn cur_state(&mut self) -> &mut dyn State<P>;
 
-    fn dispatch(&mut self, msg: &Box<P>) {
+    fn dispatch(&mut self, msg: &P) {
         log::debug!("StateMachine<P>::dispatch:+");
         self.cur_state().process(msg);
         log::debug!("StateMachine<P>::dispatch:-");
     }
 }
 
-#[allow(unused)]
+// Trait for States
 pub trait State<P> {
-    fn enter(&mut self, msg: &Box<P>) {
+    /// Invoked when first transitioning to the state
+    fn enter(&mut self, msg: &P) {
+        let _ = msg; // Keep the signature clean, pretend to use it here
         log::debug!("State: enter");
     }
-    fn process(&mut self, msg: &Box<P>) {
-        log::debug!("State: process:");
+
+    /// Invoked on every message
+    fn process(&mut self, msg: &P) {
+        let _ = msg; // Keep the signature clean, pretend to use it here
+        log::debug!("State: process");
     }
-    fn exit(&mut self, msg: &Box<P>) {
+
+    /// Invoked when transitioning to a state, this includes if the transtion
+    /// is to the current state, in which case exit then enter will be invoked.
+    fn exit(&mut self, msg: &P) {
+        let _ = msg; // Keep the signature clean, pretend to use it here
         log::debug!("State: exit");
     }
 }
 
+// Create a Protocal with two messages
 #[derive(Debug)]
 enum Protocol1 {
     Msg1 { f1: i32 },
@@ -30,21 +43,23 @@ enum Protocol1 {
 }
 
 struct MySm {
-    cur_state: Box<dyn State<Protocol1> + 'static>,
+    current_state: Box<dyn State<Protocol1> + 'static>,
 }
 
+//impl<'a> StateMachine<Protocol1> for MySm<'a> {
 impl StateMachine<Protocol1> for MySm {
     fn cur_state(&mut self) -> &mut Box<dyn State<Protocol1> + 'static> {
         log::debug!("MySm::StateMachine::cur_state():+-");
-        &mut self.cur_state
+        &mut self.current_state
     }
 }
 
 struct State1;
 
 impl State<Protocol1> for State1 {
-    fn process(&mut self, msg: &Box<Protocol1>) {
-        match **msg {
+    fn process(&mut self, msg: &Protocol1) {
+        #[allow(unused)]
+        match *msg {
             Protocol1::Msg1 { f1 } => {
                 log::debug!("State1: process Msg1::f1={}", f1);
             }
@@ -58,7 +73,7 @@ impl State<Protocol1> for State1 {
 struct State2;
 
 impl State<Protocol1> for State2 {
-    fn process(&mut self, msg: &Box<Protocol1>) {
+    fn process(&mut self, msg: &Protocol1) {
         log::debug!("State2: process msg={:?}", msg);
     }
 }
@@ -67,21 +82,24 @@ fn main() {
     env_logger_init("info");
     log::info!("Hello, world!");
 
-    let state1 = State1 {};
-    let state2 = State2 {};
-
+    // Create state machine and initialize to State1
     let mut mysm = MySm {
-        cur_state: Box::new(state1),
+        //current_state: Box::new(state1),
+        current_state: Box::new(State1 {}),
     };
 
-    let msg = Box::new(Protocol1::Msg1 { f1: 123 });
-    mysm.cur_state.enter(&msg);
+    // Allocate a message on the stack and dispatch it
+    let msg = Protocol1::Msg1 { f1: 123 };
+    mysm.current_state.enter(&msg);
     mysm.dispatch(&msg);
-    mysm.cur_state.exit(&msg);
+    mysm.current_state.exit(&msg);
 
-    mysm.cur_state = Box::new(state2);
-    let msg2 = Box::new(Protocol1::Msg2 { f1: "a string" });
-    mysm.cur_state.enter(&msg2);
+    // Transition to State2
+    mysm.current_state = Box::new(State2 {});
+
+    // Allocate a second message on the heap and enter, dispatch msg then exit the state
+    let msg2 = Protocol1::Msg2 { f1: "a string" };
+    mysm.current_state.enter(&msg2);
     mysm.dispatch(&msg2);
-    mysm.cur_state.exit(&msg2);
+    mysm.current_state.exit(&msg2);
 }
